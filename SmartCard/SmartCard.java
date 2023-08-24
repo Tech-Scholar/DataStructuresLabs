92ddac0a79c172b1f9048269f3b0f47c6485680f
George F <63172033+Tech-Scholar@users.noreply.github.com>
2023-08-23 20:50:41 -0400

    Create SmartCard.java

---
+++ SmartCard/SmartCard.java
@@ -0,0 +1,64 @@
+public class SmartCard {
+  private boolean onboard;
+  private double balance;
+  private Station station;
+  public SmartCard(double balance){
+    this.balance = balance;
+    this.station = null;
+    this.onboard = false;
+  }
+  public double getBalance(){
+    return balance;
+  }
+  public boolean getIsBoarded(){
+    return onboard;
+  }
+  public Station getBoardedAt(){
+    return station;
+  }
+  public String getFormattedBalance(){
+    return String.format("$%.2f", balance);
+  }
+  public void board(Station s){
+    if (onboard){
+      System.out.println("Error: already boarded?!");
+    }
+    else if (balance < 0.50){
+      System.out.println("Insufficient funds to board. Please add more money.");
+    }
+    else {
+        station = s;
+        onboard = true;
+    }
+  }
+  public double cost(Station s){
+    int start_zone = station.getZone();
+    int end_zone = s.getZone();
+    double cost = 0.50;
+    for (int i = start_zone; i < end_zone; i++){
+      cost += 0.75;
+    }
+    return cost;
+  }
+  public void exit(Station s){
+    if (!onboard){
+      System.out.println("Error: Did not board?!");
+    }
+    else {
+      double cost = cost(s);
+      if (cost > balance){
+        System.out.println("Insufficient funds to exit. Please add more money.");
+      }
+      else {
+        balance -= cost;
+        onboard = false;
+        System.out.println(String.format("From %s to %s costs $%.2f. SmartCard has $%.2f",station.getName(), s.getName(), cost, balance));
+        station = null;
+      }
+    }
+  }
+  public void addMoney(double d){
+    balance += d;
+    System.out.println(String.format("$%.2f added. Your new balance is $%.2f", d, balance));
+  }
+}
